98c64ddddd8619ba28717862ed9769ad8bf8e6e8
Donato Montrone <114031046+donatomontrone@users.noreply.github.com>
2026-09-02 20:47:10 +0200

    Remove unused MagicMock import
    Removed unused import of MagicMock from unittest.mock.

--- tests/test_device_manager.py
+++ tests/test_device_manager.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from unittest.mock import MagicMock
-
 from custom_components.bticino_myhome.device import BticinoDeviceManager
 from custom_components.bticino_myhome.discovery import DiscoveredDevice
 
